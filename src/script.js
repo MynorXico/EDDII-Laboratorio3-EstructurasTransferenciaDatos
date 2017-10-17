5c37b17520bdf6d6a0b3b4f0cf585642bb369058
Mynor Xico <mynorottoniel.xico2@gmail.com>
2017-10-17 17:52:24 -0600

    Mejor presentación en form

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 
 function clearJSON(){
 	
-	var strJSON = '{"added-items":[]}';
+	var strJSON = '{"key-value-pairs":[]}';
 	localStorage.setItem("JSON", strJSON);
 	alert("Se limpiaron los registros");
+
+	document.getElementById("output").innerHTML="";
 	
 }
 function addRegister(){
@@ -17,14 +19,15 @@ function addRegister(){
 	var a = localStorage.getItem("JSON");
 	if(a==null){
 		alert("Se creará JSON");
-		var strJSON = '{"added-items":[]}';
+		var strJSON = '{"key-value-pairs":[]}';
 		localStorage.setItem("JSON", strJSON);
 	}
 	// Se asume que ya existe
 	var JSON_obj = JSON.parse(localStorage.getItem("JSON"));
-	JSON_obj["added-items"].push(JSONItem(new_key, new_value));
+	JSON_obj["key-value-pairs"].push(JSONItem(new_key, new_value));
 	var strJSON = JSON.stringify(JSON_obj);
 	localStorage.setItem("JSON", strJSON);
+	alert("El registro se agregó correctamente");
 }
 function showJSON(){
 	document.getElementById("output").innerHTML = "Hola";
@@ -33,6 +36,7 @@ function showJSON(){
 		return;
 	}
 	document.getElementById("output").innerHTML = localStorage.getItem("JSON");
+	return false;
 }
 
 
