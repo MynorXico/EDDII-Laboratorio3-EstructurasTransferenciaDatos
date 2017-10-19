079113bdc766377493f725c3f13a10aaf0845cfb
SpitfireMunguia <davidmunguiaamericas@gmail.com>
2017-10-18 20:15:01 -0600

    JSON2XML arreglado

--- src/script.js
+++ src/script.js
@@ -41,6 +41,7 @@ function showJSON(){
 	}
 	if(document.getElementById('xmlb').checked)
 	{
+	
 		var str= JSON.stringify( localStorage.getItem("JSON"));
 		var o=JSONtoXML(str);
 		document.getElementById("output").innerHTML = o;
@@ -55,27 +56,22 @@ function showJSON(){
 }
 function JSONtoXML(json_object)
 {
-	var flag=localStorage.getItem("XML");
-	if(flag==null)
-	{
-		alert("Se creara la esctructura xml");
-		var strXML="";
-		localStorage.setItem("XML",strXML);
-	}
+	json_object=json_object.replace(/\\/g,"");
+	json_object=json_object.replace(/\"/g, "");
 	//crar el xml 
 	var header='&lt;?xml version="1.0" encoding="UTF-8"?>'+'<br/>';
 	var counter=json_object.length;
 	var auxlenght=0;
-	var masterCloseTag='';
+	var finaltag='';
 	for(var i=0;i<counter;i++)
 	{
 		
 		if(json_object.charAt(i)=='[')//la primera llave
 		{
-			var JSONname='&lt;'+json_object.substring(2,auxlenght-2)+'>'+'<br/>';//lo que va donde iria <catalog>
-		     masterCloseTag=JSONname;
-			var allValues=json_object.substring(auxlenght);//quito mas porqueria
-			allValues=allValues.substring(1,allValues.length-2);//quito mas porqueria
+			finaltag=json_object.substring(1,auxlenght-2);
+			var JSONname='&lt;'+json_object.substring(1,auxlenght-2)+'>'+'<br/>';//lo que va donde iria <catalog>
+			var allValues=json_object.substring(auxlenght);
+			allValues=allValues.substring(1,allValues.length-2);
 			var pairs=allValues.split("}");//separo lo que van a ser las tags
 			var xmlBody=header+JSONname+'\n';
 			for(var j=0;j<pairs.length-1;j++)//limpiar cada par de llave-valor
@@ -83,18 +79,16 @@ function JSONtoXML(json_object)
 				var keyvalue=pairs[j];
 				keyvalue=j!=0?keyvalue.substring(2):keyvalue.substring(1);
 				var chain=keyvalue.split(",");//se tiene los pares llave valor
-				var tag="<"+(chain[0].split(":")[1])+">";//la cosas que abren y cierran
-				var closetag=tag.substring(0,tag.length-2)+"/>" ;
-				var tagvalue=((tag+chain[1].split(":")[1]+closetag));//las cosas que tienen adentro las tags
+				var tag='&lt;'+(chain[0].split(":")[1])+">";//la cosas que abren y cierran
+				var closetag=tag.substring(0,tag.length-1)+"/>" ;
+				var tagvalue='&nbsp;'+'&nbsp;'+'&nbsp;'+((tag+chain[1].split(":")[1]+closetag));//las cosas que tienen adentro las tags
 				xmlBody+=tagvalue+'<br/>';
 			}
 			break;
 		}
 		auxlenght++;
 	}
-	masterCloseTag=masterCloseTag.substring(0,masterCloseTag.length-2)+'/>';
-	xmlBody+=masterCloseTag;
-	alert(xmlBody);	
+	xmlBody+='&lt'+finaltag+'/>';	
 	return xmlBody;
 	
 }
